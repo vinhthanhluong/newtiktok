fd86f8133dc1441c60bae1b99adbe78af977ef43
thành <vinhthanhluong@gmail.com>
2024-08-21 22:05:46 +0700

    Set Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import { publicRoutes } from '~/Routes';
+
 function App() {
     return (
         <div className="App">
-            <h1>Theme Default</h1>
+            <Router>
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        return <Route key={index} path={route.path} element={<Page />} />;
+                    })}
+                </Routes>
+            </Router>
         </div>
     );
 }
---
+++ src/pages/Following/index.js
@@ -0,0 +1,5 @@
+function Following() {
+    return <h1>Page Following</h1>;
+}
+
+export default Following;
