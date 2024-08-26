283730bfdea9e1edc52737427fbec61c9a7924e7
thành <vinhthanhluong@gmail.com>
2024-08-26 23:25:13 +0700

    Set header search #1

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,8 @@
 const images = {
     logo: require('~/assets/images/logo.svg').default,
     noImg: require('~/assets/images/no-img.png'),
+    iClose: require('~/assets/images/ic-close.svg').default,
+    iSearch: require('~/assets/images/ic-search.svg').default,
 };
 
 export default images;
--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -1,12 +1,37 @@
 import clsx from 'clsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faSpinner, faXmark } from '@fortawesome/free-solid-svg-icons';
+import TippyHeadless from '@tippyjs/react/headless';
+import 'tippy.js/dist/tippy.css';
 
 import styles from './Search.module.scss';
+import images from '~/assets/images';
 
 function Search() {
     return (
-        <div className={clsx(styles.search)}>
-            <span>search2</span>
-        </div>
+        <TippyHeadless
+            visible
+            interactive={true}
+            placement="bottom"
+            render={(attrs) => (
+                <div className={clsx(styles.searchResult)} tabIndex="-1" {...attrs}>
+                    My tippy box
+                </div>
+            )}
+        >
+            <div className={clsx(styles.wrapper)}>
+                <input type="text" placeholder="Search..." className={clsx(styles.ipt)} />
+                <button className={clsx(styles.clearn)}>
+                    <FontAwesomeIcon icon={faXmark} />
+                </button>
+                <span className={clsx(styles.loading)}>
+                    <FontAwesomeIcon icon={faSpinner} />
+                </span>
+                <button className={clsx(styles.btn)}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </div>
+        </TippyHeadless>
     );
 }
 
